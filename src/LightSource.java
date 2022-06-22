d4065a2c09417dbb36313f980d6fb1cc30ad132e
Vaishak K <vaishakk@gmail.com>
2022-06-22 13:10:14 +0200

    Fix LightSource constructor

--- src/LightSource.java
+++ src/LightSource.java
@@ -3,9 +3,9 @@ import org.javatuples.Pair;
 public class LightSource {
     Pair <Integer, Integer> position;
     Pair <Integer, Integer> velocity;
-    public LightSource(int x, int y, int vx, int vy){
-        this.position = Pair.with(x, y);
-        this.velocity = Pair.with(vx, vy);
+    public LightSource(Pair<Integer, Integer> position, Pair<Integer, Integer> velocity){
+        this.position = position;
+        this.velocity = velocity;
     }
 
     public Pair<Integer, Integer> getPosition(){
