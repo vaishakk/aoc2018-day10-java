349cd3d831bbb344e8c3b2a1353745262309d121
Vaishak K <vaishakk@gmail.com>
2022-06-22 12:46:47 +0200

    Update LightSource

--- src/LightSource.java
+++ src/LightSource.java
@@ -2,12 +2,17 @@ import org.javatuples.Pair;
 
 public class LightSource {
     Pair <Integer, Integer> position;
-
-    public LightSource(int x, int y){
+    Pair <Integer, Integer> velocity;
+    public LightSource(int x, int y, int vx, int vy){
         this.position = Pair.with(x, y);
+        this.velocity = Pair.with(vx, vy);
     }
 
     public Pair<Integer, Integer> getPosition(){
         return this.position;
+    }
+
+    public Pair<Integer, Integer> getVelocity(){
+        return this.velocity;
     }
 }
