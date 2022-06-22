797fe83fe06437316db1fbee1b19a7894ee31391
Vaishak K <vaishakk@gmail.com>
2022-06-22 12:52:42 +0200

    Add movement for LightSource

--- src/LightSource.java
+++ src/LightSource.java
@@ -15,4 +15,14 @@ public class LightSource {
     public Pair<Integer, Integer> getVelocity(){
         return this.velocity;
     }
+
+    public void move_one_step(){
+        this.position = Pair.with(this.position.getValue0() * this.velocity.getValue0(),
+                this.position.getValue1() * this.velocity.getValue1()) ;
+    }
+
+    public void move_many_steps(int steps){
+        this.position = Pair.with(this.position.getValue0() * steps * this.velocity.getValue0(),
+                this.position.getValue1() * steps * this.velocity.getValue1()) ;
+    }
 }
